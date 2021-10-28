44aefab525ebbf083a946837abb34aa362ecc73a
Nabeel Naiyer <naiyer.nabeel@gmail.com>
2021-10-28 13:55:40 -0500

    Updates

---
+++ precache-manifest.ff67e5fc0fddb96bad81044a824a9177.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "087541bf2b30bc80d72c4c3776adc64e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "265c04df3741303356a8",
+    "url": "/static/css/main.b67f3c58.chunk.css"
+  },
+  {
+    "revision": "63f69aa644d0a9c2f1d3",
+    "url": "/static/js/2.d5b8e08a.chunk.js"
+  },
+  {
+    "revision": "48a224ba09bfc96df4e3426c2ec529aa",
+    "url": "/static/js/2.d5b8e08a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "265c04df3741303356a8",
+    "url": "/static/js/main.d2103237.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "6e9ddbdf6c14fe14e66585375e752e23",
+    "url": "/static/media/NabeelNaiyer_SiteResume.6e9ddbdf.pdf"
+  },
+  {
+    "revision": "dc1571b29a30ef6f3cba211d9980f7b0",
+    "url": "/static/media/Report.dc1571b2.pdf"
+  },
+  {
+    "revision": "eccb27065c3bc47b0cc84465de4a9b93",
+    "url": "/static/media/nintendo.eccb2706.png"
+  },
+  {
+    "revision": "d7d6aa478d864bd68b2aab5a4278a6a6",
+    "url": "/static/media/peppers.d7d6aa47.png"
+  },
+  {
+    "revision": "45378aff5f0b0138b91c719a60d6f2a3",
+    "url": "/static/media/profile.45378aff.png"
+  },
+  {
+    "revision": "9d333e1ab0e69b61b32161a38ea2dcf8",
+    "url": "/static/media/tbd.9d333e1a.png"
+  },
+  {
+    "revision": "ceb0eb10fab9be3147610d73a4a0e02d",
+    "url": "/static/media/vrtx.ceb0eb10.png"
+  }
+]);
